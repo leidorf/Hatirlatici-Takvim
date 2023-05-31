6a59e497144f7a7d15b9e8c5345e49145e6ad4e1
Güray Dağ <dagguray@gmail.com>
2023-05-31 15:12:54 +0300

    Giriş Ekranı düzeltmeleri

--- Kodlar/Js.js
+++ Kodlar/Js.js
@@ -1,15 +1,25 @@
+window.addEventListener("DOMContentLoaded", function() {
   var loginForm = document.getElementById("login-form");
   var signupForm = document.getElementById("signup-form");
 
-  // Sayfa yüklendiğinde varsayılan olarak giriş yapma formunu göster
-  showLoginForm();
+  loginForm.style.display = "block";
+  signupForm.style.display = "none";
+});
 
-  function showLoginForm() {
+function toggleForm() {
+  var loginForm = document.getElementById("login-form");
+  var signupForm = document.getElementById("signup-form");
+  var toggleButton = document.getElementById("toggle-button");
+
+  if (loginForm.style.display === "none") {
     loginForm.style.display = "block";
     signupForm.style.display = "none";
-  }
-
-  function showSignupForm() {
+    toggleButton.innerHTML = "Giriş Yap / Kayıt Ol";
+    toggleButton.style.backgroundColor = "#379b6d";
+  } else {
     loginForm.style.display = "none";
     signupForm.style.display = "block";
+    toggleButton.innerHTML = "Kayıt Ol / Giriş Yap";
+    toggleButton.style.backgroundColor = "rgb(107, 188, 219)";
   }
+}
